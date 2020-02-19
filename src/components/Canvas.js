a0f889db48c5f31bcb11e661351eb2f97ed06f5d
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-02-19 01:20:16 -0500

    finished animation

---
+++ src/components/Canvas.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import styled, {keyframes} from 'styled-components'
+import Section from '../styles/Section.js'
+import cover from '../images/cover.jpg'
+
+
+const fade = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+
+const Wrapper = styled(Section)`
+  position: absolute;
+  width: calc(100% - 32px);
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row-reverse;
+  flex-wrap: wrap-reverse; 
+  pointer-events: none;
+  opacity: 1;
+  overflow: hidden; 
+  @media (max-width: 1200px){
+    width: 100%;
+  }
+`;
+
+const Block = styled.div`
+  display: flex; 
+  flex-direction: ${props => props.blockDirection};
+  min-width: ${props => props.blockWidth};
+  max-height: 8.5%;
+  min-height: 8.5%;
+  overflow: hidden; 
+`;
+
+
+
+const Cell = styled.div`
+  width: 100%;
+  height: 100%; 
+  min-width: ${props => props.cellWidth};
+  min-height: ${props => props.cellHeight};
+  background-image: url(${cover});
+  background-size: 800%;
+  background-position-x: ${props => props.cellX};
+  background-position-y: ${props => props.cellY};
+  animation: ${fade} .5s linear;
+  animation-delay: ${props => props.delay};
+  opacity: 0;
+  animation-fill-mode: forwards;
+`;
+
+
+
+
+function Canvas(){
+   
+    const blocks = [];
+
+    let x = 95;
+    let y = 95; 
+    let base = 0;
+
+    for(let i = 0; i < 170; i ++) {
+        //cell quantity
+        const r1 = Math.floor(Math.random() * 3 + 3);
+        //block direction
+        const r2 = Math.round(Math.random());
+        //block width
+        const r3 = Math.round(Math.random());
+
+        const cells = [];
+        
+        for(let j = 0; j < r1; j ++) {
+        //cell size
+        const r4 = Math.floor(Math.random() * 3);
+        //x position 
+        const r5 = Math.floor(Math.random() * 3);
+        //y position 
+        const r6 = Math.floor(Math.random() * 3);
+        //delay
+        const r7 = Math.floor(Math.random() * 3);
+        cells.push({
+            maxWidth: r2 === 0 ? ['10%','20%','30%'][r4] : '100%',
+            maxHeight: r2 === 1 ? ['10%','20%','40%'][r4] : '100%',
+            posX: `${[x - 40, x, x + 40][r5]}%`,
+            posY: `${[y - 10, y, y + 10][r6]}%`,
+            key: j,
+            delay: [base, base + .5, base + 1][r7]
+        })
+        }
+
+        x -= 7;
+        if(x < 0) x = 95;
+        if((i%14 === 0) && (i > 13)) y -= 7.5;
+        if((i%14 === 0) && (i > 13)) base += .25;
+
+        blocks.push(
+        <Block blockDirection={r2 === 0 ? 'row' : 'column'} blockWidth={r3 === 0 ? '5%' : '8%'} key={i} id={i}>
+            {cells.map(cell => {
+                return(
+                <Cell key={`${i}${cell.key}`} delay={`${cell.delay}s`} cellX={cell.posX} cellY={cell.posY} cellWidth={cell.maxWidth} cellHeight={cell.maxHeight}  background={`rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`}/>
+                )
+            })}
+        </Block>
+        )
+    }
+ 
+
+  return (
+      <Wrapper> 
+        {blocks}
+      </Wrapper>
+  )
+}
+
+export default Canvas
--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, {useEffect, useState} from 'react'
+import styled, {keyframes} from 'styled-components'
 import Section from '../styles/Section.js'
-import cover from '../images/cover.png'
+import cover from '../images/cover.jpg'
+import Canvas from './Canvas.js'
+
 
 const Wrapper = styled(Section)`
   background-image: linear-gradient(
@@ -12,95 +14,72 @@ const Wrapper = styled(Section)`
   );
 `;
 
-const Canvas = styled(Section)`
-  position: absolute;
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row-reverse;
-  flex-wrap: wrap-reverse; 
-  // background-color: red;
+const Overlay = styled(Section)`
+  background-image: linear-gradient(
+    to bottom, 
+    rgba(0,35,90,1) 0%, 
+    rgba(50,120,160,.5) 50%, 
+    rgba(255,200,0,1) 100%
+  );
+  width: calc(100% - 32px);
+  position: absolute; 
+  filter: saturate(120%);
   pointer-events: none;
-  opacity: .5;
-  // overflow-hidden; 
+  @media (max-width: 1200px){
+    width: 100%;
+  }
 `;
 
-const Block = styled.div`
-  display: flex; 
-  flex-direction: ${props => props.blockDirection};
-  width: ${props => props.blockWidth};
-  max-height: 8.5%;
-  min-height: 8.5%;
-  overflow: hidden; 
-`;
 
-const Cell = styled.div`
-  width: 100%;
-  height: 100%; 
-  min-width: ${props => props.cellWidth};
-  min-height: ${props => props.cellHeight};
+const Image = styled(Overlay)`
   background-image: url(${cover});
-  // background-color: ${props => props.background};
-
-  background-size: 1000%;
-  background-position-x: ${props => props.cellX};
-  background-position-y: ${props => props.cellY};
+  background-size: cover; 
+  background-position: center; 
 `;
 
+const spin = keyframes`
+  0% { opacity: .75; transform: rotate(0deg); }
+  100% { opacity: .75; transform: rotate(360deg); }
+`;
 
+const Loader = styled.div`
+  border: 5px solid black;
+  border-top: 5px solid white;
+  margin: 0 auto;
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  animation: ${spin} 2s linear;
+  animation-iteration-count: 2;
+  opacity: 0;
+`;
+
+const LoaderWrap = styled(Section)`
+  display: flex;  
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background: red;
+`;
 
 function Cover(){
 
-  const blocks = []; 
-  let x = 95;
-  let y = 95; 
+  const [delay, updateDelay] = useState(false);
 
-  for(let i = 0; i < 170; i ++) {
-    //cell quantity
-    const r1 = Math.floor(Math.random() * 3 + 3);
-    //block direction
-    const r2 = Math.round(Math.random());
-    //block width
-    const r3 = Math.round(Math.random());
-
-    const cells = [];
-    
-    for(let j = 0; j < r1; j ++) {
-      //cell size
-      const r4 = Math.floor(Math.random() * 3);
-      //x position 
-      const r5 = Math.floor(Math.random() * 3);
-      //y position 
-      const r6 = Math.floor(Math.random() * 3);
-      cells.push({
-        maxWidth: r2 === 0 ? ['10%','20%','30%'][r4] : '100%',
-        maxHeight: r2 === 1 ? ['10%','20%','40%'][r4] : '100%',
-        posX: `${[x - 35, x, x + 35][r5]}%`,
-        posY: `${[y - 5, y, y + 5][r6]}%`,
-        key: j
-      })
-    }
-   
-    x -= 7;
-    if(x < 0) x = 95;
-    if((i%14 === 0) && (i > 13)) y -= 7.5;
-
-    blocks.push(
-      <Block blockDirection={r2 === 0 ? 'row' : 'column'} blockWidth={r3 === 0 ? '5%' : '10%'} key={i} id={i}>
-          {cells.map(cell => {
-             console.log(i, cell.posX, cell.posY)
-            return(
-              <Cell key={`${i}${cell.key}`} cellX={cell.posX} cellY={cell.posY} cellWidth={cell.maxWidth} cellHeight={cell.maxHeight}  background={`rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`}/>
-            )
-          })}
-      </Block>
-    )
-  }
+  useEffect(()=> {
+    setTimeout(()=>updateDelay(true), 100);
+  },[delay])
+ 
 
   return (
     <Wrapper>
-      <Canvas> 
-        {blocks}
-      </Canvas>
+      <Image/>
+      {delay && <Canvas/>}
+      <Overlay/>
+      <LoaderWrap>
+        <Loader/>
+      </LoaderWrap>
     </Wrapper>
   )
 }
