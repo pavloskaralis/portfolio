36a5e51bf2bfa4a67537238b8dec7021f42d172b
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-05-03 22:09:19 -0400

    auto scroll

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -102,7 +102,7 @@ function Cover(){
         let root = document.querySelector('#root')
         let container = useRef(document.querySelector('#container') || root);
         // console.log('test',window.innerHeight)
-        let scrollHeight = container.current.scrollHeight > 0 ? container.current.scrollHeight : window.innerHeight * 8;
+        let scrollHeight = container.current.scrollHeight > 0 ? container.current.scrollHeight : window.innerHeight * 9;
 
         const [maxHeight, setMaxHeight] = useState(scrollHeight);
         const [scroll, setScroll] = useState(container.current.scrollTop)
@@ -142,8 +142,8 @@ function Cover(){
             background={ scroll > 0 ? '25,70,85,.85' : '0,0,0,0' } 
             shadow={ scroll > 0 ? '0 5px 10px 0 rgba(0,0,0,.25)' : 'none' }
         >
-            <Title href='/' color={ (scroll >= (maxHeight * .10) && scroll <= (maxHeight * .22)) || (scroll >= (maxHeight * .73) && scroll <= (maxHeight * .86)) ? 'black' : 'white' }><div>Pavlos<span>Karalis</span></div></Title>
-            <LinkWrap color={ (scroll >= (maxHeight * .10) && scroll <= (maxHeight * .22)) || (scroll >= (maxHeight * .73) && scroll <= (maxHeight * .86)) ? 'black' : 'white' }>
+            <Title href='/' color={ (scroll >= (maxHeight * .11) && scroll <= (maxHeight * .22)) || (scroll >= (maxHeight * .77) && scroll <= (maxHeight * .88)) ? 'black' : 'white' }><div>Pavlos<span>Karalis</span></div></Title>
+            <LinkWrap color={ (scroll >= (maxHeight * .11) && scroll <= (maxHeight * .22)) || (scroll >= (maxHeight * .77) && scroll <= (maxHeight * .88)) ? 'black' : 'white' }>
                 <a href='/#about'>About</a>
                 <a href='/#portfolio'>Portfolio</a>
                 <a href='/#technologies'>Technologies</a>
